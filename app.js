4cfbb3c141ae0bd0479a5e3f4daa24f3193d23da
jcn13 <jcn13@uol.com.br>
2018-01-22 14:13:44 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+firebase.initializeApp({
+	databaseURL: 'https://dht11capstone.firebaseio.com/'
+})
+
+const db = firebase.database()
+const ref = db.ref('sensors')
+let keys
+let k
+
+function getDB(){ 
+  ref.on('value', data => { 
+    let html = '<div class="display"><table><tr>'
+    let values = data.val()
+    let keys = Object.keys(values)
+    console.log(keys)
+    for (let i = 0; i < keys.length; i++) {
+      k = keys[i]
+      let temp = values[k].temperature
+      let hum = values[k].humidity
+      let date = values[k].timestamp      
+    }
+    let seconds = values[k].timestamp
+    let d = new Date(seconds*1000)
+    console.log(values[k])
+ 	html += '<tr><td>Temperature</td><td>' + values[k].temperature +' *C</td></tr>'
+	html += '<tr><td>Humidity</td><td>' + values[k].humidity +' %</td></tr>'
+	html += '<tr><td>Date</td><td>' + d +'</td></tr>'
+    html += '</tr></table></div>'
+    document.getElementById('history').innerHTML = html     
+  })
+}
+
+getDB()
